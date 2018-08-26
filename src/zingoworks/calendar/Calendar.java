39e53a13be9a08604f540294bc26636a2914c338
zingoworks <zingoworks@DESKTOP-3DHOO3S>
2018-08-27 03:49:18 +0900

    Update - using class method

--- src/zingoworks/calendar/Calendar.java
+++ src/zingoworks/calendar/Calendar.java
@@ -3,25 +3,38 @@ package zingoworks.calendar;
 import java.util.Scanner;
 
 public class Calendar {
+	//최대일수 구하는 로직 함수로 빼기 < 재사용 가능
+	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	
-	public static void main(String[] args) {
+	public int getMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month - 1];
+	}
+	
+	public void printSampleCalendar() {
 		System.out.println(" 일      월      화      수      목      금      토");
 		System.out.println("--------------------------");
 		System.out.println(" 1   2   3   4   5   6   7");
 		System.out.println(" 8   9  10  11  12  13  14");
 		System.out.println("15  16  17  18  19  20  21");
 		System.out.println("22  23  24  25  26  27  28");
+	}
+	
+	
+	public static void main(String[] args) {
+
 		
 		
 		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
 		System.out.println("달을 입력하세요.");		
 		int month = scanner.nextInt();
 		
-		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 		
-		System.out.printf("%d월은 %d일까지 있습니다.\n", month, maxDays[month-1]);
+		System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
 		scanner.close();
+		
+		cal.printSampleCalendar();
 	}
 	
 }
